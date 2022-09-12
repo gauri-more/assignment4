740f78f4e9ef922f4664979688a3c82fee6cae20
Gauri Wakchaure <gwakchaure1122@gmail.com>
2022-09-12 11:29:28 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+    jQuery("#carousel-case-studies").owlCarousel({
+        autoplay: false,
+        rewind: true,
+        margin: 20,
+        responsiveClass: true,
+        autoHeight: true,
+        autoplayTimeout: 7000,
+        smartSpeed: 800,
+        nav: false,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+
+            600: {
+                items: 1
+            },
+
+            1024: {
+                items: 3
+            },
+
+            1366: {
+                items: 3
+            }
+        }
+    });
+    jQuery("#carousel-testimonial").owlCarousel({
+        autoplay: false,
+        rewind: true,
+        margin: 20,
+        responsiveClass: true,
+        autoHeight: true,
+        autoplayTimeout: 7000,
+        smartSpeed: 800,
+        nav: false,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+
+            600: {
+                items: 1
+            },
+
+            1024: {
+                items: 2
+            },
+
+            1366: {
+                items: 2
+            }
+        }
+    });
+})
